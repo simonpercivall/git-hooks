127c61f18263037762dbc60850816be32acd84e1
Simon Percivall <simon.percivall@oneflow.com>
2017-09-18 18:20:16 +0200

    Update git-hooks.rb Homebrew formula.

--- git-hooks.rb
+++ git-hooks.rb
@@ -1,8 +1,13 @@
 require 'formula'
 
 class GitHooks < Formula
+  desc "Manage project, user, and global Git hooks"
   homepage 'https://github.com/simonpercivall/git-hooks'
-  version '1.0'
+
+  url "https://github.com/simonpercivall/git-hooks/archive/v1.0+sp2.tar.gz"
+  version "1.0_sp2"
+  sha256 "38b0098384de02af8d4299f841d96fb5a08ef40bbaa63366e149d45853e596cd"
+
   head 'https://github.com/simonpercivall/git-hooks.git'
   
   def install
